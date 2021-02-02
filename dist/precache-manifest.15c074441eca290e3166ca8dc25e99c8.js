e0c2b8cd011ede12a5245fcad176712f3fc2f0a0
Matthew Mckenzie <matthewmckenzie@Matthews-MacBook-Pro.local>
2021-02-01 23:34:27 -0600

    All loaders and plugins installed

---
+++ dist/precache-manifest.15c074441eca290e3166ca8dc25e99c8.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711baceabca1a57bb5655397a7fad605",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d72dfb0227b7b2ce9f33",
+    "url": "main.css"
+  },
+  {
+    "revision": "d72dfb0227b7b2ce9f33",
+    "url": "main.js"
+  }
+]);
