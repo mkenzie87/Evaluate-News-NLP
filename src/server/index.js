3241ad1ef7f2ba526cf9cc5903dbd948d2d4b236
Matthew Mckenzie <matthewmckenzie@Matthews-MacBook-Pro.local>
2021-02-01 22:45:16 -0600

    Adding all my files before API

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,67 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+var https = require('follow-redirects').https;
+var fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config();
+
+var json = {
+    'title': 'test json response',
+    'message': 'this is a message',
+    'time': 'now'
+}
 
 const app = express()
+app.use(cors())
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.use(express.static('dist'))
 
-console.log(__dirname)
+console.log(JSON.stringify(mockAPIResponse))
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
-
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+    res.sendFile('dist/index.html')
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    res.json(mockAPIResponse);
 })
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
+})
+
+var options = {
+    'method': 'POST',
+    'hostname': 'api.meaningcloud.com',
+    'path': '/sentiment-2.1?key=13d2bf320cf4836f44b05ff1af01636a&lang=<lang>&txt=<text>&model=<model>',
+    'headers': {
+    },
+    'maxRedirects': 20
+  };
+
+  var req = https.request(options, function (res) {
+    var chunks = [];
+
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on("end", function (chunk) {
+      var body = Buffer.concat(chunks);
+      console.log(body.toString());
+    });
+
+    res.on("error", function (error) {
+      console.error(error);
+    });
+  });
